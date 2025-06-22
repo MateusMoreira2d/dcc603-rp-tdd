eb49e64dd53a85db1122e1bcfe1d3e31681eb34e
Mateus.2d <104143361+MateusMoreira2d@users.noreply.github.com>
2025-06-22 11:32:07 -0300

    Quarto commit

--- Money.java
+++ Money.java
@@ -1,17 +1,28 @@
 public abstract class Money {
     protected int amount;
+    private String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount &&
+               getClass().equals(money.getClass());
+    }
+
+    public String currency() {
+        return currency;
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int multiplier);
